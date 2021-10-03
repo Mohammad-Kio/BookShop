17a7ed9c1c0de61af48a730c9806a40e77a8cd64
mohammad mustafa <mohammad.utdcompany@gmail.com>
2021-10-03 16:29:34 +0300

    Add project files.

---
+++ MVC/Data/AddDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC.Data
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> op): base(op)
+        {
+
+        }
+    }
+}
